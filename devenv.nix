b2be23341953d840666adfb0e8eb85c1960c267f
Muxutruk2 <156070698+Muxutruk2@users.noreply.github.com>
2025-11-09 11:28:25 +0100

    Trying to fix some rust prolems

--- devenv.nix
+++ devenv.nix
@@ -1,13 +1,26 @@
 { pkgs, ... }:
 
 {
-  packages = with pkgs; [ openssl ];
+  packages = with pkgs; [
+    openssl
+    rust-analyzer
+    rustPlatform.rustLibSrc
+  ];
 
   env.RUST_LOG = "debug,reqwest=info,hyper=info,cookie_store=info";
 
-  languages.rust.enable = true;
+  languages.rust = {
+    enable = true;
+
+    components = [
+      "rustc"
+      "cargo"
+      "clippy"
+      "rustfmt"
+      "rust-analyzer"
+    ];
+  };
   git-hooks.hooks = {
     rustfmt.enable = true;
-    clippy.enable = true;
   };
 }
